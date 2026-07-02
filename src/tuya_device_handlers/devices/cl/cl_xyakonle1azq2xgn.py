cb490fdc33bc218032346722d69dfdc0555d5d7c
Lucas Mindêllo de Andrade <lucas@mindello.com.br>
2026-07-02 00:59:16 -0300

    Add Tuya blind motor quirk for xyakonle1azq2xgn
    Update quirk documentation for Tuya blind motor to clarify that it reports percent_state as 0 and requires ignoring certain data points for control.

---
+++ src/tuya_device_handlers/devices/cl/cl_xyakonle1azq2xgn.py
@@ -0,0 +1,18 @@
+"""Quirk for Tuya blind motor (product_id xyakonle1azq2xgn).
+
+This device does reports ``percent_state`` always as 0.
+
+To be able to control the device, we need to ignore the 
+``percent_control`` and ``percent_state`` DPs.
+"""
+
+from tuya_device_handlers import TUYA_QUIRKS_REGISTRY
+from tuya_device_handlers.builder import DeviceQuirk
+
+(
+    DeviceQuirk()
+    .applies_to(product_id="xyakonle1azq2xgn")
+    .remove_dpid(dpid=9, dpcode="percent_control")
+    .remove_dpid(dpid=8, dpcode="percent_state")
+    .register(TUYA_QUIRKS_REGISTRY)
+)
